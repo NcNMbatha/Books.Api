81d63f328c48a2b6ef5ecda7d9e14af16f2d74bf
Njabulo Nhlanhla Mbatha <mbathan1@legal.regn.net>
2024-07-09 18:54:36 +0200

    Register Autofac module

--- Books.Api/Program.cs
+++ Books.Api/Program.cs
@@ -1,8 +1,19 @@
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using Books.Api.IoC;
+using Books.Api.Utils;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory())
+       .ConfigureContainer<ContainerBuilder>(builder =>
+       {
+           builder.RegisterModule(new AutofacModule(new ConfigUtils()));
+       });
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
