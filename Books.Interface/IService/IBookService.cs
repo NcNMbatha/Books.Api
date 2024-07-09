aced8602e64a1a3f9a41f2fc841dee32d03d12ed
Njabulo Nhlanhla Mbatha <mbathan1@legal.regn.net>
2024-07-09 20:14:27 +0200

    Book Service

--- Books.Interface/IService/IBookService.cs
+++ Books.Interface/IService/IBookService.cs
@@ -1,6 +1,17 @@
-﻿namespace Books.Interface.IService
+﻿using Books.Model;
+
+namespace Books.Interface.IService
 {
     public interface IBookService
     {
+        Task<int> CreateBookAsync(Book book);
+
+        Task<Book> GetBookByIdAsync(int id);
+
+        Task<IEnumerable<Book>> GetAllBooksAsync();
+
+        Task<int> UpdateBookAsync(Book book);
+
+        Task<int> DeleteBookAsync(int id);
     }
 }
--- Books.Service/BookService.cs
+++ Books.Service/BookService.cs
@@ -1,7 +1,40 @@
-﻿namespace Books.Service
-{
-    public class BookService
-    {
+﻿using Books.Interface.IRepository;
+using Books.Interface.IService;
+using Books.Model;
 
+namespace Books.Service
+{
+    public class BookService:IBookService
+    {
+        private readonly IBookRepository _bookRepository;
+        public BookService(IBookRepository bookRepository)
+        {
+            _bookRepository = bookRepository;
+        }
+
+        public async Task<int> CreateBookAsync(Book book)
+        {
+            return await _bookRepository.CreateBookAsync(book);
+        }
+
+        public async Task<Book> GetBookByIdAsync(int id)
+        {
+            return await _bookRepository.GetBookByIdAsync(id);
+        }
+
+        public async Task<IEnumerable<Book>> GetAllBooksAsync()
+        { 
+            return await _bookRepository.GetAllBooksAsync();
+        }
+
+        public async Task<int> UpdateBookAsync(Book book)
+        {
+            return await _bookRepository.UpdateBookAsync(book);
+        }
+
+        public async Task<int> DeleteBookAsync(int id)
+        {
+            return await _bookRepository.DeleteBookAsync(id);
+        }
     }
 }
